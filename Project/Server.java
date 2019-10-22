c022ce7c4b0c5e1bd20b67bd5ef46403d9454e5b
MarcoTurzi <56305514+MarcoTurzi@users.noreply.github.com>
2019-10-22 19:15:45 +0200

    Add files via upload

--- Project/Server.java
+++ Project/Server.java
@@ -1,94 +1,56 @@
 import java.io.*;
+import java.io.IOException;
 import java.net.*;
 
 public class Server {
-	
-	
-	public static void main(String args[])
-	{
-		int port=-1;
-		try {
-		port=Integer.parseInt(args[0]);
-			if(port<1023||port>49152)
-			{
-				//messaggio errore e chiuditi
-			}
+	public static final int PORT = 1026;
+	public static void main(String[] args) {
+		
+		int port = -1;
+		FileUtility utility = new FileUtility();
+		if (args.length == 0) {
+			
+			port = PORT;
+			
+		}else {try {
+			port = Integer.parseInt(args[0]);
+		} catch (NumberFormatException e) {
+			System.err.println("Errore formato porta (intero)");
+			System.exit(1);
+			e.printStackTrace();
+		}}
+		
+		ServerSocket serverSocket = null;
+		
 		
 			
-		}catch(NumberFormatException e)
-		{
-			//stacktrace e chiudi
-		}
-		
-	
-	
-	//creare connessione
-		try {
-		ServerSocket Ssocket=new ServerSocket(port);
-		Socket socket;
-		DataInputStream dataIn;
-		DataOutputStream dataOut;
-		String fileName;
-		File file;
-		int fileLength=0;
-		
-
-		//attendi stream
-		while(true)
-		{
-			socket=Ssocket.accept();
-			
-			
-			
-			//verifica che il file non sia presente
-			
-			dataIn=new DataInputStream(socket.getInputStream());
-			dataOut=new DataOutputStream(socket.getOutputStream());
-			fileName=dataIn.readUTF();
-			file=new File(fileName);
-			if(file.exists())
-			{
+			try {
+				serverSocket = new ServerSocket(port);
+			} catch (IOException e) {
+				System.err.println("Errore Socket");
+				System.exit(2);
 				
-				//manda il messaggio "salta file"
-				dataOut.writeUTF("salta file");
+			}
+			
+			while(true) {
+				
+				Socket clientSocket = null;
+				try {
+					System.out.println("Inizio");
+					clientSocket = serverSocket.accept();
+				} catch (IOException e) {
+					System.err.println("Errore accept");
+					System.exit(3);
+				}
+				
+				new DirectoryServer(clientSocket).run();
+				
+				
+				
 				
 				
 			}
-			else
-			{
-	
-				//manda "accetta"
-				dataOut.writeUTF("accetta");
-				
-				
-				
-				//attendi lunghezza del file
-				fileLength=Integer.parseInt(dataIn.readUTF());
-				
-				
-				//genera thread con socket nomefile e length
-				
-				
-			}
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		}
-		catch(Exception e)
-		{
 			
-		}
-	
-	
-	
-		
-		
 		
 	}
 
